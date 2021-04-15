eb017432a95bfd4c299da71aae71a44debb95519
AJTech2002 <Ajay Venkat>
2021-04-15 11:19:07 +1000

    feat: base classes

---
+++ lib/base.dart
@@ -0,0 +1,35 @@
+class Place {
+
+  String address;
+  String name;
+  String type;
+
+  LatLon position;
+  
+  //All out of 5
+  double socialDistancingScore;
+  double cleanlinessScore;
+  double staffFriendlinessScore;
+
+  //Other ratings ...
+  
+
+  Place (String address, String type, LatLon pos)
+  {
+    this.address = address;
+    this.type = type;
+    this.position = pos;
+  }
+  
+}
+
+class LatLon {
+  double lat,lon;
+
+  LatLon (double _lat, double _lon)
+  {
+    this.lat = _lat;
+    this.lon = _lon;
+  }
+
+}
