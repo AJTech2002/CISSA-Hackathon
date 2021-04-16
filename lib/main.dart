325172daa87db99d9408e6b274a4950451d4190a
Zhaolinguan <zhaoling@student.unimelb.edu.au>
2021-04-17 00:52:33 +1000

    initial gps page

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'base.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(GPS());
 }
 
-class MyApp extends StatelessWidget {
+class GPS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: GPSPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  //Hello
-  final String title;
-
+class GPSPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GPSPageState createState() => _GPSPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GPSPageState extends State<GPSPage> {
+  var locationMessage = "";
 
-  void _incrementCounter() {
-    
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<LatLon> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double long = position.longitude;
+    String latString = lat.toStringAsFixed(6);
+    String longString = long.toStringAsFixed(6);
+    setState(() {
+      locationMessage = "Latitude: $latString, Longitude: $longString";
+    });
+    return LatLon(lat, long);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Location Services"),
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.location_on,
+                size: 80.0,
+                color: Colors.red,
+              ),
+              Text(locationMessage),
+              RaisedButton(
+                onPressed: () {
+                  _getCurrentLocation();
+                },
+                color: Colors.blue[700],
+                child: Text("Get My Current Location",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+              )
+            ]),
       ),
     );
   }
